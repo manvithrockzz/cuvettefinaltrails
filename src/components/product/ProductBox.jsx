a11cba66613e2251caebda87b114398e115e9ab8
manvithrockzz <kottamanvith@gmail.com>
2023-06-20 21:19:55 +0530

    Update code- fourth update

--- src/components/product/ProductBox.jsx
+++ src/components/product/ProductBox.jsx
@@ -9,56 +9,56 @@ import { UserContext } from '../../App';
 export default
     function ProductBox(props) {
 
-    const [displayChips, setDisplayChips] = useState();
-    const [displayComments, setDisplayComments] = useState();
-    const [showCommentBox, setShowCommentBox] = useState();
-    const [comment, setComment] = useState('');
-    const [likeCount, setLikeCount] = useState();
-    const [commentCount, setCommentCount] = useState();
+    const [isChipsVisible, setisChipsVisible] = useState();
+    const [commentDisplayState, setCommentDisplayState] = useState();
+    const [isBoxCommentVisible, setIsBoxCommentVisible] = useState(); 
+    const [commentText, setCommentText] = useState('');
+    const [numLikes, setNumLikes] = useState();
+    const [totalComments, setTotalComments] = useState();
     const { userLoggedIn, setModalToShow, setView, setProductToEdit } = useContext(UserContext);
 
     useEffect(() => {
-        const tempDisplayChips = props.tags.map((item) => {
+        const isChipsShown = props.tags.map((item) => {
             return (
                 <span className={styles.chipItems}>{item}</span>
             )
         })
 
-        const tempDisplayComments = props.comments.map((item) => {
+        const isCommentsDisplayed = props.comments.map((item) => {
             return (
                 <span className={styles.comments}>{item}</span>
             )
         })
 
-        setDisplayComments(tempDisplayComments);
-        setDisplayChips(tempDisplayChips);
-        setShowCommentBox(false);
-        setLikeCount(props.likes);
-        setCommentCount(props.comments_count);
+        setCommentDisplayState(isCommentsDisplayed);
+        setisChipsVisible( isChipsShown);
+        setIsBoxCommentVisible(false);
+        setNumLikes(props.likes);
+        setTotalComments(props.comments_count);
     }, [])
 
 
-    const handleCommentBox = () => {
-        setShowCommentBox(showCommentBox ? false : true);
+    const BoxtoggleComment = () => {
+        setIsBoxCommentVisible(isBoxCommentVisible ? false : true);
     }
 
-    const writeComment = (e) => {
-        setComment(() => {
+    const handleCommentInputChange = (e) => {
+        setCommentText(() => {
             return e.target.value
 
         })
     }
 
-    const handleComment = async () => {
+    const handleSubmitComment = async () => {
         const result = await Postcomment({
             id: props.id,
-            comment: comment
+            comment: commentText
         })
         if (result.success) {
-            setCommentCount(commentCount + 1);
-            const newCommentsArray = displayComments.map(item => item)
-            newCommentsArray.push(<span className={styles.comments}>{comment}</span>)
-            setDisplayComments(newCommentsArray);
+            setTotalComments(totalComments + 1);
+            const newCommentsArray = commentDisplayState.map(item => item)
+            newCommentsArray.push(<span className={styles.comments}>{commentText}</span>)
+            setCommentDisplayState(newCommentsArray);
         }
         else {
             toast.error(result.message, { autoClose: 2000 })
@@ -68,7 +68,7 @@ export default
     const handleLikes = async () => {
         const result = await Upvote(props.id);
         if (result.success) {
-            setLikeCount(likeCount + 1);
+            setNumLikes(numLikes + 1);
         }
         else {
             toast.error(result.message, { autoClose: 2000 })
@@ -87,39 +87,42 @@ export default
 
     return (
         <>
-            <div className={styles.main}>
-                <img src={props.logo} className={styles.image1}></img>
-                <div className={styles.box2}>
-                    <span className={styles.text1}>{props.name}</span>
-                    <span className={styles.text2}> {props.description}</span>
-                    <div className={styles.box21}>
-                        {displayChips}
-                        <img src="../../Images/IconC2.png" alt="" className={styles.image3} onClick={handleCommentBox} />
-                        <span className={styles.text4} onClick={handleCommentBox}>Comment</span>
+            <div className={styles.ProductBoxContainer}>
+                <img src={props.logo} className={styles.HomeImage}></img>
+                <div className={styles.mainDiv}>
+                    <span className={styles.HeadingOne}>{props.name}</span>
+                    <span className={styles.HeadingTwo}> {props.description}</span>
+                    <div className={styles.productBoxDiv}>
+                        {isChipsVisible}
+                        <img src="../../Images/IconC2.png" alt="" className={styles.commentIcon} onClick={BoxtoggleComment} />
+                        <span className={styles.text4} onClick={BoxtoggleComment}>Comment</span>
                         {userLoggedIn && <span className={`${styles.button1} `} onClick={handleEdit}>Edit</span>}
                     </div>
                 </div>
                 <div className={styles.box3}>
                     <div className={styles.box31} onClick={handleLikes}>
                         <img src='../../Images/Upvote.png' className={styles.image2}></img>
-                        <span className={styles.text3}>{likeCount}</span>
+                        <span className={styles.text3}>{numLikes}</span>
                     </div>
                     <div className={styles.box32}>
-                        <span className={styles.text5}>{commentCount}</span>
+                        <span className={styles.text5}>{totalComments}</span>
                         <img src="../../Images/IconC1.png" alt="" className={styles.image5} />
                     </div>
                 </div>
 
             </div>
-            {showCommentBox && <div className={styles.main2}>
+            {isBoxCommentVisible && <div className={styles.main2}>
                 <div className={styles.top}>
-                    <input className={styles.commentBox} placeholder='Add a comment...' onChange={writeComment}></input>
-                    <img src="../../Images/Submit.png" alt="" className={styles.image4} onClick={handleComment} />
+                    <input className={styles.commentBox} placeholder='Add a comment...' onChange={handleCommentInputChange}></input>
+                    <img src="../../Images/Submit.png" alt="" className={styles.image4} onClick={handleSubmitComment} />
                 </div>
                 <div className={styles.bottom}>
-                    {displayComments}
+                    {commentDisplayState}
                 </div>
             </div>}
         </>
     )
 }
+
+
+
